63c76ed496b6eed2d03a63812fe9e4c1b3084bd3
Vanessa Bertoldo <126096822+Vanessa-Bertoldo@users.noreply.github.com>
2024-06-11 21:04:30 -0300

    Criação do calendario

--- app/src/main/java/br/com/fiap/challenge/MainActivity.kt
+++ app/src/main/java/br/com/fiap/challenge/MainActivity.kt
@@ -1,46 +1,20 @@
 package br.com.fiap.challenge
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import br.com.fiap.challenge.ui.theme.ChallengeTheme
+import androidx.annotation.RequiresApi
+import br.com.fiap.challenge.activitys.CalendarActivity
+
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ChallengeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        startActivity(Intent(this, CalendarActivity::class.java))
+        finish()
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ChallengeTheme {
-        Greeting("Android")
-    }
-}
